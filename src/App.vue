695ca045067da9f833187357528bd837da1243a6
ryomak <ryooomaaa0413@outlook.com>
2018-07-26 20:29:38 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <header-component/>
-    <img src="./assets/logo.png">
     <top/>
     <footer-component/>
   </div>
--- src/components/top/contact.vue
+++ src/components/top/contact.vue
@@ -34,14 +34,15 @@
                      :max-rows="6">
     	</b-form-textarea>
       </b-form-group>
-      <b-button type="submit" variant="secondly">Submit</b-button>
-      <b-button type="reset" variant="secondly">Reset</b-button>
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="primary">Reset</b-button>
     </b-form>
     </section>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
 export default {
   name:"Contact",
   data () {
@@ -58,6 +59,7 @@ export default {
     onSubmit (evt) {
       evt.preventDefault();
       alert(JSON.stringify(this.form));
+      this.postform(this.form);
     },
     onReset (evt) {
       evt.preventDefault();
@@ -68,6 +70,34 @@ export default {
       /* Trick to reset/clear native browser form validation state */
       this.show = false;
       this.$nextTick(() => { this.show = true });
+    },
+    postform(v){
+      var form = document.createElement('form');
+      var mail_r = document.createElement('input');
+      var title_r = document.createElement('input');
+      var content_r = document.createElement('input');
+   
+      form.method = 'POST';
+      form.action = 'https://docs.google.com/forms/d/e/1FAIpQLSdScSitSpdVGNIGq1C7z56AWANOxj6bBHaS-99TacXK2QEH3Q/formResponse';
+   
+      mail_r.type = 'hidden'; //入力フォームが表示されないように
+      mail_r.name = 'entry.2040554687';
+      mail_r.value = v.email;
+
+      title_r.type = 'hidden'; //入力フォームが表示されないように
+      title_r.name = 'entry.787095462';
+      title_r.value = v.title;
+
+      content_r.type = 'hidden'; //入力フォームが表示されないように
+      content_r.name = 'entry.1722078225';
+      content_r.value = v.content;
+   
+      form.appendChild(mail_r);
+      form.appendChild(title_r);
+      form.appendChild(content_r);
+      document.body.appendChild(form);
+   
+      form.submit();
     }
   }
 }
